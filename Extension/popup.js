0cb5984351c267053ccad6b406e4f96ce46d8f53
Conor Weng <90535796+JiaxiongWeng-Conor@users.noreply.github.com>
2023-03-25 10:21:32 -0400

    Update popup.js

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,4 +1,25 @@
-document.getElementById('generateReport').addEventListener('click', async () => {
-  // Call ChatGPT API to generate a meeting report
-  // ...
+document.getElementById("generateReport").addEventListener("click", async () => {
+  const meetingNotes = document.getElementById("meetingNotes").value;
+  const resultDiv = document.getElementById("result");
+
+  if (meetingNotes.trim()) {
+    resultDiv.innerHTML = "Generating report...";
+    try {
+      const response = await fetch("https://your-backend-service.com/chatgpt", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ meeting_notes: meetingNotes })
+      });
+
+      const data = await response.json();
+      resultDiv.innerHTML = data.report || "Error: Unable to generate report";
+    } catch (error) {
+      console.error("Error:", error);
+      resultDiv.innerHTML = "Error: Unable to generate report";
+    }
+  } else {
+    resultDiv.innerHTML = "Please enter your meeting notes";
+  }
 });
